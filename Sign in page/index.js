749e990d04341a4cfa03e24e2fc148f622d82148
KutsiGiira <haytamboualkhours@gmail.com>
2024-01-08 22:19:50 +0100

    day 3

---
+++ Sign in page/index.js
@@ -0,0 +1,14 @@
+var info = document.querySelectorAll("input");
+// var vle = document.querySelectorAll("value");
+var sent = document.getElementsByTagName("button")[0];
+var value = document.getElementById("value")[0];
+
+sent.addEventListener("click", ()=>{
+    if(input.value.length > 0){
+        alert("failed");
+    }
+    else
+    {
+        alert("succes");
+    }
+})
