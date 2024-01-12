c672d83662532374d6da9fbc06cbd15b9391758d
KutsiGiira <haytamboualkhours@gmail.com>
2024-01-12 13:44:42 +0100

    making it a little better (0 as sec number value not acceptable and some css added)

--- Calculator/cal.js
+++ Calculator/cal.js
@@ -1,7 +1,6 @@
 var first = document.querySelector("#first");
 var second = document.querySelector("#second");
 var result = document.querySelector("span");
-
 var divide = document.getElementsByTagName("button")[0];
 var multiply = document.getElementsByTagName("button")[1];
 
@@ -11,14 +10,16 @@ var secondv;
 divide.addEventListener("click", () => {
     firstv = first.value;
     secondv = second.value;
-    result.innerHTML = parseFloat(firstv) / parseFloat(secondv);
-});
-
+        if(secondv == 0){
+        result.innerHTML = "Second number value can't be 0";
+        }
+        else{
+            result.innerHTML = firstv + " / "  + secondv + " = " + parseFloat(firstv) / parseFloat(secondv);
+        }
+})
 multiply.addEventListener("click", ()=>{
     firstv = first.value;
     secondv = second.value;
-    result.innerHTML = parseFloat(firstv)* parseFloat(secondv);
+    result.innerHTML = firstv + " * "  + secondv + " = " + parseFloat(firstv)* parseFloat(secondv);
 })
-
-
 
