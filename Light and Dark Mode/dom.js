8b5d2286e96f6fffe8ff80f56e6f580f327b15ba
KutsiGiira <haytamboualkhours@gmail.com>
2024-01-13 13:50:49 +0100

    now it can be used multiple times

--- Light and Dark Mode/dom.js
+++ Light and Dark Mode/dom.js
@@ -1,18 +1,25 @@
-var btn = document.querySelector(".btn");
+function remode(){
+    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector("h2").style.color = "white";
+    document.querySelector(".container").style.borderColor = "white";
+    document.querySelector("h2").innerHTML = "Dark Mode";
+    document.querySelector(".text").style.color = "white";
+    document.querySelector(".top").style.borderBottomColor = "white";
+    document.querySelector(".text").style.borderColor = "white";
+    document.querySelector("#black").style.display = "none";
+    document.querySelector("#white").style.display = "block"; 
+}
 
-btn.addEventListener("click", ()=>{
-    if(btn.classList.remove("top")){
+
+function mode(){
         document.querySelector("body").style.backgroundColor = "white";
         document.querySelector("h2").style.color = "black";
         document.querySelector(".container").style.borderColor = "black";
         document.querySelector("h2").innerHTML = "Ligt Mode";
         document.querySelector(".text").style.color = "black";
         document.querySelector(".top").style.borderBottomColor = "black";
-        document.querySelector(".text").style.borderColor = "black"; 
-    }
-    else{
-        (btn.classList.remove(".text"));
-    }
-})
-
-alert("Click the button at End Rigt for light mode");
+        document.querySelector(".text").style.borderColor = "black";
+        document.querySelector("#black").style.display = "block";
+        document.querySelector("#white").style.display = "none";     
+     
+}
